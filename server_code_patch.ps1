07ba9fbe428c84622b62b4ec0ff54795fe55ef50
杨黄林 <yhl452493373@gmail.com>
2024-03-13 20:20:46 +0800

    Create server_code_patch.ps1

---
+++ server_code_patch.ps1
@@ -0,0 +1,30 @@
+Param(
+    [Parameter(Mandatory = $false)]
+    $SourceDir = "./Server"
+)
+
+# 去除IP限制
+$FILE_PATH = "$SourceDir/project/assets/configs/http.json"
+$SEARCH_STRING = "127.0.0.1"
+$REPLACE_STRING = "0.0.0.0"
+# 读取文件内容
+$FILE_CONTENT = Get-Content -Path $FILE_PATH
+# 替换指定字符串
+$NEW_CONTENT = $FILE_CONTENT.replace($SEARCH_STRING, $REPLACE_STRING)
+# 将修改后的内容写回文件
+$NEW_CONTENT | Set-Content -Path $FILE_PATH
+# 打印日志
+Write-Output "Replace $SEARCH_STRING with $REPLACE_STRING in file $FILE_PATH"
+
+# 去除IP限制
+$FILE_PATH = "$SourceDir/project/assets/configs/core.json"
+$SEARCH_STRING = "`"giveCommandEnabled`": false"
+$REPLACE_STRING = "`"giveCommandEnabled`": true"
+# 读取文件内容
+$FILE_CONTENT = Get-Content -Path $FILE_PATH
+# 替换指定字符串
+$NEW_CONTENT = $FILE_CONTENT.replace($SEARCH_STRING, $REPLACE_STRING)
+# 将修改后的内容写回文件
+$NEW_CONTENT | Set-Content -Path $FILE_PATH
+# 打印日志
+Write-Output "Replace $SEARCH_STRING with $REPLACE_STRING in file $FILE_PATH"
