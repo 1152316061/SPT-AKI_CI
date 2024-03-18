8b85708a1f48504309456c56f486a45088f56ed5
杨黄林 <yhl452493373@gmail.com>
2024-03-18 09:25:44 +0800

    Update server_code_patch.ps1

--- server_code_patch.ps1
+++ server_code_patch.ps1
@@ -22,14 +22,14 @@ if ($SIT) {
 }
 
 # 开启 spt give 命令
-$FILE_PATH = "$SourceDir/project/assets/configs/core.json"
-$SEARCH_STRING = "`"giveCommandEnabled`": false"
-$REPLACE_STRING = "`"giveCommandEnabled`": true"
+#$FILE_PATH = "$SourceDir/project/assets/configs/core.json"
+#$SEARCH_STRING = "`"giveCommandEnabled`": false"
+#$REPLACE_STRING = "`"giveCommandEnabled`": true"
 # 读取文件内容
-$FILE_CONTENT = Get-Content -Path $FILE_PATH
+#$FILE_CONTENT = Get-Content -Path $FILE_PATH
 # 替换指定字符串
-$NEW_CONTENT = $FILE_CONTENT.replace($SEARCH_STRING, $REPLACE_STRING)
+#$NEW_CONTENT = $FILE_CONTENT.replace($SEARCH_STRING, $REPLACE_STRING)
 # 将修改后的内容写回文件
-$NEW_CONTENT | Set-Content -Path $FILE_PATH
+#$NEW_CONTENT | Set-Content -Path $FILE_PATH
 # 打印日志
-Write-Output "Replace $SEARCH_STRING with $REPLACE_STRING in file $FILE_PATH"
+#Write-Output "Replace $SEARCH_STRING with $REPLACE_STRING in file $FILE_PATH"
